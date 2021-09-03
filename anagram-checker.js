315846e35e0dca35aa6a8e5531969b6b30f5a392
fobadara <62938735+fobadara@users.noreply.github.com>
2021-09-03 16:03:43 +0100

    Add bugs

---
+++ anagram-checker.js
@@ -0,0 +1,5 @@
+module.exports = function (str) {
+    // write your code here
+    return stri.replace(/[^0-9a-z]/gi, '').toLowerCase() = str.replace(/[^0-9a-z]/gi, '').split("").reverse().join("").toLowerCase();
+  }
+  
